eb19b436d51fce5de1d3fe2f86619583d106528a
sama-kun <samgar.robot@gmail.com>
2024-02-29 16:07:28 +0600

    Add Aptfile

--- routers/synonymizer.js
+++ routers/synonymizer.js
@@ -2,7 +2,6 @@ const express = require("express");
 const app = express();
 const router = express.Router();
 const axios = require("axios");
-const { JSDOM } = require("jsdom");
 require("dotenv").config();
 
 router.get("/", async (req, res) => {
@@ -47,78 +46,42 @@ async function synonymSearch(word) {
 }
 
 async function performSearch(word) {
-  try {
-    // Создаем виртуальное окружение браузера с помощью jsdom
-    const dom = await JSDOM.fromURL(process.env.WIPO_URL);
+  const browser = await puppeteer.launch({
+    executablePath:
+      ".cache/puppeteer/chrome/linux-122.0.6261.69/chrome-linux64/chrome", // Set the path to Chrome executable
+    headless: true, // Or false if you want to see the browser window
+  });
+  const page = await browser.newPage();
+  const url = process.env.WIPO_URL;
 
-    // Получаем объекты для работы с DOM
-    const document = dom.window.document;
-    const inputElement = document.querySelector("#searchInputBox");
-    const buttonElement = document.querySelector("#searchButton");
-    console.log(inputElement.value, buttonElement);
-
-    // Устанавливаем значение в инпут
-    inputElement.value = word;
-
-    // Имитируем клик по кнопке
-    buttonElement.click();
-
-    // Ждем загрузки результатов поиска
-    await new Promise((resolve) => setTimeout(resolve, 2000)); // Пауза в 2 секунды (можно изменить)
-
-    // Обрабатываем результаты поиска
+  await page.goto(url);
+  await page.type("#searchInputBox", word);
+  await page.click("#searchButton");
+  await page.waitForSelector("#divHitList ul");
+  const searchResults = await page.evaluate(() => {
     const resultList = [];
-    document.querySelectorAll("#divHitList ul li").forEach((element) => {
-      const cls = element.getAttribute("cls");
-      const classBadge = element.querySelector(".classBadge").textContent;
-      const text = element.textContent.replace(classBadge, "").trim();
-      console.log(text, cls);
+    const items = document.querySelectorAll("#divHitList ul li");
+    items.forEach((item) => {
+      const cls = item.getAttribute("cls");
+      const classBadge = item.querySelector(".classBadge").innerText;
+      const text = item.innerText.replace(classBadge, "").trim();
       resultList.push({ cls, text });
     });
-
     return resultList;
-  } catch (error) {
-    console.error("Error occurred during search:", error);
-  }
+  });
+  await browser.close();
+  return searchResults;
 }
-
-// async function performSearch(word) {
-//   const browser = await puppeteer.launch({
-//     executablePath:
-//       ".cache/puppeteer/chrome/linux-122.0.6261.69/chrome-linux64/chrome", // Set the path to Chrome executable
-//     headless: true, // Or false if you want to see the browser window
-//   });
-//   const page = await browser.newPage();
-//   const url = process.env.WIPO_URL;
-
-//   await page.goto(url);
-//   await page.type("#searchInputBox", word);
-//   await page.click("#searchButton");
-//   await page.waitForSelector("#divHitList ul");
-//   const searchResults = await page.evaluate(() => {
-//     const resultList = [];
-//     const items = document.querySelectorAll("#divHitList ul li");
-//     items.forEach((item) => {
-//       const cls = item.getAttribute("cls");
-//       const classBadge = item.querySelector(".classBadge").innerText;
-//       const text = item.innerText.replace(classBadge, "").trim();
-//       resultList.push({ cls, text });
-//     });
-//     return resultList;
-//   });
-//   await browser.close();
-//   return searchResults;
-// }
 
 router.get("/:word", async (req, res) => {
   try {
     const word = req.params.word;
     // Call the function to perform the search
     const wipo = await performSearch(word);
-    // const synonyms = await synonymSearch(word);
+    const synonyms = await synonymSearch(word);
     // Send the search results as response
     console.log(wipo);
-    res.json({ word, wipo });
+    res.json({ word, wipo, synonyms });
   } catch (error) {
     console.log(error);
     res.status(500).send("Internal Server Error");
