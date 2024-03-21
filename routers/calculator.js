1291d7e74d4ee440cb24d4e358c8057a741aab3e
sama-kun <samgar.robot@gmail.com>
2024-03-21 23:26:07 +0500

    finished

--- routers/calculator.js
+++ routers/calculator.js
@@ -5,6 +5,9 @@ const auth = require("../middleware/auth");
 const countryModel = require("../models/countryModel");
 const countries = require("../seeds/countries.json");
 const axios = require("axios");
+const country = require("../models/countryModel");
+const ordinaryCountries = countries.ordinary;
+const renewalCountries = countries.renewal_ordinary;
 
 const serviceByType = {
   new: {
@@ -27,21 +30,87 @@ const serviceByType = {
 
 router.get("/", async (req, res) => {
   try {
-    console.log(countries.ordinary.length);
+    console.log(countries.countries.length);
     // res.send(countries.countries.length);s
   } catch (error) {
     console.log(error);
   }
 });
 
-// router.get("/", async (req, res) => {
-//   try {
-//     console.log(countries.ordinary.length);
-//     // res.send(countries.countries.length);s
-//   } catch (error) {
-//     console.log(error);
-//   }
-// });
+router.get("/setordinary", async (req, res) => {
+  try {
+    let resi = [];
+    for (let code of renewalCountries) {
+      const country = await countryModel.updateOne(
+        { code },
+        { $set: { renewalType: "ordinary" } }
+      );
+
+      if (!country) {
+        resi.push(code);
+      }
+    }
+    // const len = await countryModel.find({ type: "ordinary" });
+
+    // for (let count of len) {
+    //   resi.push(count.code);
+    // }
+    // console.log(countries.ordinary.length);
+    res.send(resi);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+router.get("/compareordinary", async (req, res) => {
+  try {
+    let resi = [];
+    // for (let code of renewalCountries) {
+    //   const country = await countryModel.updateOne(
+    //     { code },
+    //     { $set: { renewalType: "ordinary" } }
+    //   );
+
+    //   if (!country) {
+    //     resi.push(code);
+    //   }
+    // }
+    const len = await countryModel.find({ renewalType: "ordinary" });
+
+    for (let count of len) {
+      resi.push(count.code);
+    }
+    // console.log(countries.ordinary.length);
+    res.send(resi);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+router.get("/getcountries", async (req, res) => {
+  try {
+    let resi = [];
+    // for (let code of renewalCountries) {
+    //   const country = await countryModel.updateOne(
+    //     { code },
+    //     { $set: { renewalType: "ordinary" } }
+    //   );
+
+    //   if (!country) {
+    //     resi.push(code);
+    //   }
+    // }
+    const len = await countryModel.find();
+
+    for (let count of len) {
+      resi.push({ code: count.code, name: count.name });
+    }
+    // console.log(countries.ordinary.length);
+    res.send(resi);
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 const getByCode = async (code) => {
   try {
@@ -146,8 +215,8 @@ router.post("/", async (req, res) => {
       } else if (el.type === "superordinary") {
         console.log(counter);
         resi.mark = counter;
-        resi.ordinaryOneFee = el.first * rate;
-        resi.ordinaryFee = counter * el.first * rate;
+        resi.superOneFee = el.first * rate;
+        resi.superFee = counter * el.first * rate;
         sum += counter * el.first * rate;
       } else if (el.type === "anomaly") {
         if (el.first) {
@@ -223,17 +292,52 @@ router.post("/renewal", async (req, res) => {
     const rate = await getRate();
     const result = {};
 
-    // console.log(countries);
-    let summa = 0;
     let fee = 0;
     result.basic = 903 * rate;
     result.fees = [];
+    // result.fees.ordinary = {};
+
+    // if (el.type === "ordinary") {
+    //   resi.firstFee = 100 * rate;
+    //   sum += el.first * rate;
+
+    //   if (mark > 3) {
+    //     resi.extraFee = (mark - 3) * 100 * rate;
+    //     resi.extraMark = mark - 3;
+    //     resi.extraOneFee = 100;
+    //     sum += (mark - 3) * 100 * rate;
+    //   }
+    // }
+
+    let hasOrdinary = countries.some(async (country) => {
+      const el = await getByCode(country.code);
+      el.renewalType === "ordinary";
+    });
+
+    let ordinary = {};
+    ordinary.oneFee = 100 * rate;
+    ordinary.extraFee = 0;
+    if (hasOrdinary && mark > 3) {
+      ordinary.extraFee = (mark - 3) * rate * 100;
+      ordinary.extraMark = mark - 3;
+    }
+
+    ordinary.countries = [];
+    ordinary.fee = 0;
 
     for (let country of countries) {
+      let counter = mark;
       let resi = {};
       let sum = 0;
       const el = await getByCode(country.code);
+      // hasOrdinary = el.type == "ordinary";
+      if (el.renewalType === "ordinary") {
+        ordinary.countries.push(el);
+        ordinary.fee += 100 * rate;
+        continue;
+      }
       resi.code = el.code;
+      console.log(el.code);
       if (el.renewalType === "normal") {
         if (mark > 3) {
           resi.extraMark = mark - 3;
@@ -241,41 +345,87 @@ router.post("/renewal", async (req, res) => {
           resi.extraOneFee = el.renewalUpper * rate;
           sum += (mark - 3) * el.renewalUpper * rate;
         }
-      } else if (el.renewalType === "anomaly" || el.renewalType === "second") {
+      } else if (el.renewalType === "second") {
         if (el.renewalFirst) {
           resi.firstFee = el.renewalFirst * rate;
           sum += el.renewalFirst * rate;
+          counter -= 1;
+          console.log(counter);
         }
-        if (el.renewalSecond && mark >= 2) {
+        if (el.renewalSecond && counter != 0 && mark >= 2) {
           resi.secondFee = el.renewalSecond * rate;
           sum += el.renewalSecond * rate;
+          counter -= 1;
+          console.log(counter);
         }
-        if (mark >= 3 && el.renewalThird) {
+
+        if (
+          el.renewalThird &&
+          el.renewalThird != 0 &&
+          counter != 0 &&
+          mark >= 3
+        ) {
+          console.log("third", el.code, el.renewalThird);
           resi.thirdFee = el.renewalThird * rate;
           sum += el.renewalThird * rate;
+          counter -= 1;
+          console.log(counter);
         }
-        if (mark > 3 && el.renewalUpper) {
-          resi.extraFee = (mark - 3) * el.renewalUpper * rate;
+
+        if (el.renewalUpper && counter != 0 && mark > 2) {
+          resi.extraFee = counter * el.renewalUpper * rate;
           resi.extraOneFee = el.renewalUpper * rate;
-          resi.extraMark = mark - 3;
-          sum += (mark - 3) * el.renewalUpper * rate;
+          resi.extraMark = counter;
+          sum += counter * el.renewalUpper * rate;
         }
       } else if (el.renewalType === "superordinary") {
-        resi.ordinaryFee = el.renewalFirst * rate;
-        sum += el.renewalFirst * rate;
+        console.log(counter);
+        resi.mark = counter;
+        resi.superOneFee = el.renewalFirst * rate;
+        resi.superFee = counter * el.renewalFirst * rate;
+        sum += counter * el.renewalFirst * rate;
+      } else if (el.renewalType === "anomaly") {
+        if (el.renewalFirst) {
+          resi.firstFee = el.renewalFirst * rate;
+          sum += el.renewalFirst * rate;
+          counter -= 1;
+          console.log(counter);
+        }
+        if (el.renewalThird && mark >= 3) {
+          console.log("third", el.code, el.renewalThird);
+          resi.thirdFee = el.renewalThird * rate;
+          sum += el.renewalThird * rate;
+          counter -= 3;
+          console.log(counter);
+        }
+        if (el.renewalUpper && counter > 0 && mark >= 2 && counter != mark) {
+          resi.extraFee = counter * el.renewalUpper * rate;
+          resi.extraOneFee = el.renewalUpper * rate;
+          resi.extraMark = counter;
+          sum += counter * el.renewalUpper * rate;
+        }
+      } else if (el.type === "free") {
+        resi.freeFee = el.renewalFirst * rate;
+        sum += resi.freeFee;
       }
-      //   console.log(el);
       resi.sum = sum;
       fee += sum;
       resi.country = el;
       result.fees.push(resi);
     }
-
+    console.log(hasOrdinary);
+    result.ordinary = ordinary;
     result.mark = mark;
     result.fee = fee;
     result.company = serviceByType[data.typeOfMark];
-    result.bank = getBank(fee);
-    result.summa = result.company.sum + result.bank + result.fee + result.basic;
+    result.bank = getBank(fee + ordinary.fee + ordinary.extraFee);
+    result.summa =
+      result.company.sum +
+      result.bank +
+      result.fee +
+      result.basic +
+      ordinary.fee +
+      ordinary.extraFee;
 
     console.log("Result: ", result);
     res.send(result);
